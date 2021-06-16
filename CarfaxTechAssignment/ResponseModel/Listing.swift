f84d730a034f3941f5154fca7a92bb76423658a6
Michael Veit <mikeveit1@gmail.com>
2021-06-16 18:54:41 -0400

    listing initial commit

--- CarfaxTechAssignment/ResponseModel/Listing.swift
+++ CarfaxTechAssignment/ResponseModel/Listing.swift
@@ -6,10 +6,27 @@
 //
 
 import Foundation
+
 struct Listing: Codable {
     let id: String
+    let images: Images
+    let year: Int
+    let make: String
+    let model: String
+    let trim: String
+    let currentPrice: Int
+    let mileage: Int
+    let dealer: Dealer
     
-    init(id: String) {
+    init(id: String, images: Images, year: Int, make: String, model: String, trim: String, currentPrice: Int, mileage: Int, dealer: Dealer) {
         self.id = id
+        self.images = images
+        self.year = year
+        self.make = make
+        self.model = model
+        self.trim = trim
+        self.currentPrice = currentPrice
+        self.mileage = mileage
+        self.dealer = dealer
     }
 }
