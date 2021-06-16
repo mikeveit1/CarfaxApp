f576229cb775e91c20207766eec118e19ce99fb3
Michael Veit <mikeveit1@gmail.com>
2021-06-16 19:10:13 -0400

    added vehicle cell file

---
+++ CarfaxTechAssignment/TableViewCells/VehicleCell.swift
@@ -0,0 +1,23 @@
+//
+//  VehicleCell.swift
+//  CarfaxTechAssignment
+//
+//  Created by Michael Veit on 6/16/21.
+//
+
+import UIKit
+
+class VehicleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
