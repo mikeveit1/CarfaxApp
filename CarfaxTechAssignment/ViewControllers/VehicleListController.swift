0e5496166b73514d58d27b578b90604adac37fe5
Michael Veit <mikeveit1@gmail.com>
2021-06-16 19:09:44 -0400

    added vehicle list controller

---
+++ CarfaxTechAssignment/ViewControllers/VehicleListController.swift
@@ -0,0 +1,19 @@
+//
+//  VehicleListController.swift
+//  CarfaxTechAssignment
+//
+//  Created by Michael Veit on 6/16/21.
+//
+
+import UIKit
+
+class VehicleListController: UIViewController {
+
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var vehicleTable: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
