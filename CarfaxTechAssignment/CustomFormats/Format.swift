323f98bc935a7c96033fa81ced7b996844138480
Michael Veit <mikeveit1@gmail.com>
2021-06-17 11:54:54 -0400

    added phone number format function

---
+++ CarfaxTechAssignment/CustomFormats/Format.swift
@@ -0,0 +1,64 @@
+//
+//  Format.swift
+//  CarfaxTechAssignment
+//
+//  Created by Michael Veit on 6/17/21.
+//
+
+import Foundation
+
+public func formatPhoneNumber(phoneNumber: String) -> String? {
+    let numbersOnly = phoneNumber.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+    let length = numbersOnly.count
+    let hasLeadingOne = numbersOnly.hasPrefix("1")
+    
+    guard length == 7 || (length == 10 && !hasLeadingOne) || (length == 11 && hasLeadingOne) else {
+        return nil
+    }
+    
+    let hasAreaCode = (length >= 10)
+    var sourceIndex = 0
+    
+    var leadingOne = ""
+    if hasLeadingOne {
+        leadingOne = "1 "
+        sourceIndex += 1
+    }
+    
+    var areaCode = ""
+    if hasAreaCode {
+        let areaCodeLength = 3
+        guard let areaCodeSubstring = numbersOnly.substring(start: sourceIndex, offsetBy: areaCodeLength) else {
+            return nil
+        }
+        areaCode = String(format: "(%@) ", areaCodeSubstring)
+        sourceIndex += areaCodeLength
+    }
+    
+    let prefixLength = 3
+    guard let prefix = numbersOnly.substring(start: sourceIndex, offsetBy: prefixLength) else {
+        return nil
+    }
+    sourceIndex += prefixLength
+    
+    let suffixLength = 4
+    guard let suffix = numbersOnly.substring(start: sourceIndex, offsetBy: suffixLength) else {
+        return nil
+    }
+    
+    return leadingOne + areaCode + prefix + "-" + suffix
+}
+
+extension String {
+    internal func substring(start: Int, offsetBy: Int) -> String? {
+        guard let substringStartIndex = self.index(startIndex, offsetBy: start, limitedBy: endIndex) else {
+            return nil
+        }
+        
+        guard let substringEndIndex = self.index(startIndex, offsetBy: start + offsetBy, limitedBy: endIndex) else {
+            return nil
+        }
+        
+        return String(self[substringStartIndex ..< substringEndIndex])
+    }
+}
