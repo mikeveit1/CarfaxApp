07ffbef592db394371826c27fd550ac3f6e6455c
Michael Veit <mikeveit1@gmail.com>
2021-06-17 10:28:30 -0400

    added set data function and phone call function

--- CarfaxTechAssignment/TableViewCells/VehicleCell.swift
+++ CarfaxTechAssignment/TableViewCells/VehicleCell.swift
@@ -9,6 +9,40 @@ import UIKit
 
 class VehicleCell: UITableViewCell {
 
+    @IBOutlet weak var listingStackView: UIStackView!
+    @IBOutlet weak var vehicleImage: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var vehicleInfoStackView: UIStackView!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var mileage: UILabel!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var dealerPhone: UIButton!
+    private var dealerPhoneNumber: String = ""
+    
+    
+    public func setData(listingData: Listing) {
+        let data = try? Data(contentsOf: URL(string: listingData.images.firstPhoto.medium)!)
+        if let imageData = data {
+             vehicleImage.image = UIImage(data: imageData)
+        } else {
+            vehicleImage.image = UIImage(named: "noimage")
+        }
+        let vehicleTitle = "\(listingData.year) \(listingData.make) \(listingData.model)"
+        title.text = vehicleTitle
+        price.text = "$\(listingData.currentPrice)"
+        mileage.text = "\(listingData.mileage) Mi"
+        let vehicleLocation = "\(listingData.dealer.city), \(listingData.dealer.state)"
+        location.text = vehicleLocation
+        dealerPhoneNumber = listingData.dealer.phone
+        dealerPhone.setTitle(listingData.dealer.phone, for: .normal)
+    }
+    
+    @IBAction func dealerPhoneButtonPressed(_ sender: Any) {
+        if let url = URL(string: "tel://\(dealerPhoneNumber)") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
