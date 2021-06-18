12b453627fcb3da7451e05bde33e61fb7623b67f
Michael Veit <mikeveit1@gmail.com>
2021-06-18 11:23:01 -0400

    removed unnecessary variable

--- CarfaxTechAssignment/DataService/DataService.swift
+++ CarfaxTechAssignment/DataService/DataService.swift
@@ -10,10 +10,7 @@ class DataService {
     static let shared = DataService()
     
     public func getData(completion: (Data) -> Void, errorHandler: (Error) -> Void) {
-        guard let path =  URL(string: "https://carfax-for-consumers.firebaseio.com/assignment.json") else { return }
-        
-        let url = path
-        
+        guard let url =  URL(string: "https://carfax-for-consumers.firebaseio.com/assignment.json") else { return }
         do {
             let data = try Data(contentsOf: url)
             completion(data)
